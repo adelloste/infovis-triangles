3166754cf3b4a6ab87b871f39a3d7e20fcdd42bf
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-07-17 18:10:19 +0200

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,62 @@
-console.log('\'Allo \'Allo!');
+/**
+ * 
+ * @param {*} data 
+ * @param {*} coordinates 
+ */
+function update(data, coordinates) {
+    console.log("/*** UPDATE ***/");
+    console.log(data);
+    console.log(coordinates);
+}
 
-// Uncomment to enable Bootstrap tooltips
-// https://getbootstrap.com/docs/4.0/components/tooltips/#example-enable-tooltips-everywhere
-// $(function () { $('[data-toggle="tooltip"]').tooltip(); });
+/**
+ * Init 
+ * @param {*} data 
+ */
+function init(data) {
+    // init keyword event
+    var evt = null;
+    // create main svg element
+    var svg = d3.select('body')
+        .on('keydown', function(k) {
+            // set keyword event
+            evt = k;
+        })
+        .on('keyup', function(k) {
+            // reset keyword event
+            evt = null;
+        })
+        .append('svg')
+        .attr('width', window.innerWidth)
+        .attr('height', window.innerHeight)
+        .attr('id', 'svg-root');
 
-// Uncomment to enable Bootstrap popovers
-// https://getbootstrap.com/docs/4.0/components/popovers/#example-enable-popovers-everywhere
-// $(function () { $('[data-toggle="popover"]').popover(); });
+    // create isosceles triangle by dataset
+    svg.append('g')
+        .selectAll('triangle')
+        .data(data)
+        .enter()
+        .append('path')
+        .attr('d', function(d) {
+            return 'M ' + d.x + ' ' + d.y + ' l ' + (d.width / 2) + ' ' + d.height + ' l -' + d.width + ' 0 z';
+        })
+        .attr('fill', function(d) { return d.tone; })
+        .attr('stroke-width', '1')
+        .attr('stroke', 'black')
+        .on('click', function(d) {
+            // update svg if the event is x
+            if(evt && evt.key === 'x') {
+                update(data, { x: d.x, y: d.y });
+            }
+            // update svg if the event is y
+            if(evt && evt.key === 'y') {
+                update(data, { x: d.x, y: d.y });
+            }
+        });
+}
+
+d3.json('/assets/stubs/triangles.json').then(
+    function(data){
+        init(data);
+    }
+);
