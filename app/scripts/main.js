8f2ef82aabf8a4571b2aebc080c46b329dd26906
Alessandro <adelloste@minsait.com>
2021-08-12 20:28:22 +0200

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 // init
 var evt    = null,
     data   = null,
+    border = 5,
     margin = { top: 10, right: 10, bottom: 10, left: 10 },
     width  = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -84,51 +85,38 @@ function updateYScaleDomain() {
  * @param {*} prop 
  */
 function update(o, coordinates, prop) {
-    // append point
-    svg.append('circle')
-        .attr('r', 4)
-        .attr('cx', coordinates.x - 10)
-        .attr('cy', coordinates.y - 10)
-        .attr('fill', 'red');
-    // get polygon
-    var polygon = points(o).map(
-        function(d) {
-            return [xScale(d.x), yScale(d.y)];
-        }
-    );
-    // check if the click is inside
-    if(d3.polygonContains(polygon, [coordinates.x - 10, coordinates.y - 10])) {
-        // the clicked point is internal so update the color
+    // base
+    if((coordinates.y - 10) >= (yScale(o.y) - border)) {
         data = d3.map(data, function(d) {
             return {
                 ...d,
-                ...(d.id !== o.id && { tone: o[prop] }),
-                ...(d.id === o.id && { [prop]: o.tone })
+                [prop]: d.width,
+                width: d[prop]
+            };
+        })
+    }
+    // check if the click is inside
+    else {
+        data = d3.map(data, function(d) {
+            return {
+                ...d,
+                [prop]: d.tone,
+                tone: d[prop]
             };
         });
     }
-    else {
-        // base
-        if((coordinates.y - 10) === yScale(o.y)) {
-            data = d3.map(data, function(d) {
-                return {
-                    ...d,
-                    ...(d.id !== o.id && { width: o[prop] }),
-                    ...(d.id === o.id && { [prop]: o.width })
-                };
-            })
-        }
-        // left or right side
-        else {
-            data = d3.map(data, function(d) {
-                return {
-                    ...d,
-                    ...(d.id !== o.id && { height: o[prop] }),
-                    ...(d.id === o.id && { [prop]: o.height })
-                };
-            })
-        }
-    }
+    // update
+    draw();
+}
+
+function updateHeight(o, coordinates, prop) {
+    data = d3.map(data, function(d) {
+        return {
+            ...d,
+            [prop]: d.height,
+            height: d[prop]
+        };
+    });
     // update
     draw();
 }
@@ -141,12 +129,14 @@ function draw() {
     updateXScaleDomain();
     updateYScaleDomain();
     // data join
-    let triangles = svg.selectAll('.triangle').data(data, function(d) {
+    let node = svg.selectAll('.node').data(data, function(d) {
         return d.id;
-    });
+    })
+    let nodeEnter = node.enter()
+        .append('g')
+        .attr('class', 'node');
     // enter clause: add new elements
-    triangles.enter()
-        .append('polygon')
+    nodeEnter.append('polygon')
         .attr('points', function(d) { 
             return points(d).map(
                 function(d) {
@@ -157,7 +147,7 @@ function draw() {
         .attr('id', function(d) { return d.id; })
         .attr('class', 'triangle')
         .attr('fill', function(d) { return d3.rgb(d.x, d.y, d.tone); })
-        .attr('stroke-width', '2')
+        .attr('stroke-width', border)
         .attr('stroke', 'black')
         .on('click', function(d, i) {
             // update svg if the event is x
@@ -168,9 +158,28 @@ function draw() {
             if(evt && evt.key === 'y') {
                 update(i, { x: d.x, y: d.y }, 'y');
             }
+        })
+    //
+    nodeEnter.append('path')
+        .attr('d', function(d) {
+            return 'M ' + xScale(d.x + d.width/2) + ' ' + yScale(d.y) +
+                ' L' + xScale(d.x + d.width/2) + ' ' + yScale(d.y + d.height) +
+                ' z'
+            }
+        )
+        .attr('class', 'triangle-height')
+        .style('stroke', 'black')
+        .style('stroke-dasharray', '10')
+        .style('stroke-width', border)
+        .on('click', function(d, i) {
+            // update svg if the event is x or y
+            if(evt && (evt.key === 'x' || evt.key === 'y')) {
+                updateHeight(i, { x: d.x, y: d.y }, evt.key);
+            }
         });
     // enter + update clause
-    triangles.transition()
+    node.select('polygon')
+        .transition()
         .duration(500)
         .attr('points', function(d) {
             return points(d).map(
@@ -180,6 +189,16 @@ function draw() {
             ).join(' ');
         })
         .attr('fill', function(d) { return d3.rgb(d.x, d.y, d.tone); });
+    // enter + update clause
+    node.select('path')
+        .transition()
+        .duration(500)
+        .attr('d', function(d) {
+            return 'M ' + xScale(d.x + d.width/2) + ' ' + yScale(d.y) +
+                ' L' + xScale(d.x + d.width/2) + ' ' + yScale(d.y + d.height) +
+                ' z'
+            }
+        );
 }
 
 // get data-cases
